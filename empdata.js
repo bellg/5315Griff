62d291c825f07758ea55a86977102391cbee3025
griff <bellg@seattleu.edu>
2018-04-30 16:35:22 -0700

    hw2 files

---
+++ empdata.js
@@ -0,0 +1,44 @@
+{
+  "data": [
+    {
+      "name": "John Stevens",
+      "designation": "Business Analyst",
+      "salary": "$170,750",
+      "joining_date": "2011/07/25",
+      "office": "Tokyo",
+      "extension": "8422"
+    },
+    {
+      "name": "Thomas Johnson",
+      "designation": "Dev Ops Engineer",
+      "salary": "$372,000",
+      "joining_date": "2012/12/02",
+      "office": "New York",
+      "extension": "4804"
+    },
+    {
+      "name": "Kelly Thompson",
+      "designation": "Senior Technical Author",
+      "salary": "$86,000",
+      "joining_date": "2009/01/12",
+      "office": "San Francisco",
+      "extension": "1562"
+    },
+    {
+      "name": "Jose Canseco",
+      "designation": "Senior Accountant",
+      "salary": "$162,700",
+      "joining_date": "2008/11/28",
+      "office": "Tokyo",
+      "extension": "5407"
+    },
+    {
+      "name": "Vladimir Jones",
+      "designation": "Pre-Sales Support",
+      "salary": "$106,450",
+      "joining_date": "2011/12/12",
+      "office": "New York",
+      "extension": "8330"
+    },
+  ]
+}
